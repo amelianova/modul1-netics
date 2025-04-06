f978e742e826c227c570045898dfdcc92dc1d56a
meloni <amelianovasafitri@gmail.com>
2025-04-06 15:53:34 +0700

    Add /health endpoint

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from fastapi import FastAPI
+from datetime import datetime
 import time
-import datetime
 import os
 
-start_time = time.time()
 app = FastAPI()
+start_time = time.time()
 
 @app.get("/health")
-def health():
-    uptime_seconds = int(time.time() - start_time)
+def health_check():
+    current_time = datetime.utcnow().isoformat()
+    uptime = round(time.time() - start_time, 2)
+
     return {
         "nama": "Tunas Bimatara Chrisnanta Budiman",
         "nrp": "5025231999",
         "status": "UP",
-        "timestamp": datetime.datetime.now().isoformat(),
-        "uptime": f"{uptime_seconds} seconds"
+        "timestamp": current_time,
+        "uptime": f"{uptime} seconds"
     }
